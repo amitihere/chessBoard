37d4c88faf835c0b86f34ee0cfee59a56be9c0dd
amitihere <amiti.aneesh1@gmail.com>
2025-11-29 01:26:47 +0530

    feat: Added Linear Gradient to create look

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ChessBoard from './src/ChessBoard';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ChessBoard />
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+        colors={['rgba(0,0,0,0.8)', 'transparent']}
+        style={styles.container}
+    >
+        <ChessBoard />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
